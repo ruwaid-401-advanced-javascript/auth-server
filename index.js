59db070868ff66525e88b64e458fe463155d0b9e
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-07 14:01:00 +0300

    initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * will start the server
+ * @module index
+ */
+
+
+// 3rd party lib
+require('dotenv').config();
+const mongoose = require('mongoose');
+// internal modules
+const server = require('./src/server.js');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth';
+const PORT = process.env.PORT || 3000;
+
+
+server.start(PORT);
+
+
+const mongooseOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions);
