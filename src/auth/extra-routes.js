b6b765c6ab824758298ca4e393024d25bbcc1be9
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-10 15:49:14 +0300

    add more routes

--- src/auth/extra-routes.js
+++ src/auth/extra-routes.js
@@ -7,9 +7,36 @@ const express = require('express');
 const router = express.Router();
 
 const bearerMiddleware = require('./middleware/bearer-auth');
+const permissions = require('./middleware/bearer-auth');
 
-router.get('/', bearerMiddleware, (req,res) => {
+
+router.get('/secret', bearerMiddleware,secretfunc);
+router.get('/read', bearerMiddleware, permissions('read'), readfunc);
+router.post('/add', bearerMiddleware, permissions('create'), addfunc);
+router.put('/change', bearerMiddleware, permissions('update'), changefunc);
+router.delete('/remove', bearerMiddleware, permissions('delete'), removefunc);
+
+
+
+function secretfunc(req,res){
   res.status(200).send('you know our secret now ---Welcome---');
-} );
+}
+
+
+function readfunc(req,res){
+  res.status(200).send('Do you see this? if yes so you have the permission to read');
+}
+
+function addfunc(req,res){
+  res.status(200).send('Do you see this? if yes so you have the permission to add');
+}
+
+function changefunc(req,res){
+  res.status(200).send('Do you see this? if yes so you have the permission to change');
+}
+
+function removefunc(req,res){
+  res.status(200).send('Do you see this? if yes so you have the permission to remove');
+}
 
 module.exports = router;
